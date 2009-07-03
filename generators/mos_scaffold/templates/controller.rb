4349c4b0d93dff48db0ba9416c8cf80e36d9422a
Reto Schüttel <reto@schuettel.ch>
2009-07-03 12:06:31 +0200

    right-system included

--- generators/mos_scaffold/templates/controller.rb
+++ generators/mos_scaffold/templates/controller.rb
@@ -2,4 +2,21 @@ class <%= controller_class_name %>Controller < ApplicationController
   make_resourceful do
     actions :all
   end
+
+  protected
+
+  def check_rights
+    case action_name
+    when "index"
+      needs_right :<%= singular_name %>_index
+    when "show"
+      needs_right :<%= singular_name %>_show
+    when "update", "edit", "new", "create"
+      needs_right :<%= singular_name %>_mgt
+    when "destroy"
+      needs_right :<%= singular_name %>_destroy
+    else
+      raise "unknown action #{action_name}"
+    end
+  end
 end
